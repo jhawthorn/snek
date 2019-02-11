574daa6712402fd9c78218bc00aea6872cb9fcba
John Hawthorn <john@hawthorn.email>
2019-02-11 01:17:45 -0800

    Fix formatting

--- web.rb
+++ web.rb
@@ -2,45 +2,45 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-    'Battlesnake documentation can be found at' \
-     '<a href=\"https://docs.battlesnake.io\">https://docs.battlesnake.io</a>.'
+  'Battlesnake documentation can be found at' \
+    '<a href=\"https://docs.battlesnake.io\">https://docs.battlesnake.io</a>.'
 end
 
 post '/start' do
-    requestBody = request.body.read
-    requestJson = requestBody ? JSON.parse(requestBody) : {}
+  requestBody = request.body.read
+  requestJson = requestBody ? JSON.parse(requestBody) : {}
 
-    # Example response
-    responseObject = {
-        "color"=> "#fff000",
-    }
+  # Example response
+  responseObject = {
+    "color"=> "#fff000",
+  }
 
-    return responseObject.to_json
+  return responseObject.to_json
 end
 
 post '/move' do
-    requestBody = request.body.read
-    requestJson = requestBody ? JSON.parse(requestBody) : {}
+  requestBody = request.body.read
+  requestJson = requestBody ? JSON.parse(requestBody) : {}
 
-    # Calculate a direction (example)
-    direction = ["up", "right"].sample
+  # Calculate a direction (example)
+  direction = ["up", "right"].sample
 
-    # Example response
-    responseObject = {
-        "move" => direction
-    }
+  # Example response
+  responseObject = {
+    "move" => direction
+  }
 
-    return responseObject.to_json
+  return responseObject.to_json
 end
 
 post '/end' do
-    requestBody = request.body.read
-    requestJson = requestBody ? JSON.parse(requestBody) : {}
+  requestBody = request.body.read
+  requestJson = requestBody ? JSON.parse(requestBody) : {}
 
-    # No response required
-    responseObject = {}
+  # No response required
+  responseObject = {}
 
-    return responseObject.to_json
+  return responseObject.to_json
 end
 
 post '/ping' do
