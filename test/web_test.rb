3bf56eda9ba9169f59de30306da1e5d9035cbb49
John Hawthorn <john@hawthorn.email>
2019-02-11 17:41:13 -0800

    Add test for /move

--- test/web_test.rb
+++ test/web_test.rb
@@ -31,4 +31,61 @@ class WebTest < MiniTest::Unit::TestCase
     post '/start'
     assert last_response.ok?
   end
+
+  FULL_MOVE_PAYLOAD = <<~JSON
+{
+  "game": {
+    "id": "game-id-string"
+  },
+  "turn": 4,
+  "board": {
+    "height": 15,
+    "width": 15,
+    "food": [
+      {
+        "x": 1,
+        "y": 3
+      }
+    ],
+    "snakes": [
+      {
+        "id": "snake-id-string",
+        "name": "Sneky Snek",
+        "health": 90,
+        "body": [
+          {
+            "x": 1,
+            "y": 3
+          }
+        ]
+      }
+    ]
+  },
+  "you": {
+    "id": "snake-id-string",
+    "name": "Sneky Snek",
+    "health": 90,
+    "body": [
+      {
+        "x": 1,
+        "y": 3
+      }
+    ]
+  }
+}
+  JSON
+
+  def test_move
+    post '/move', FULL_MOVE_PAYLOAD
+    assert last_response.ok?
+    refute_predicate last_response.body, :empty?
+
+    json = JSON.parse(last_response.body)
+
+    # It returns a valid move
+    assert_includes %w[up down left right], json['move']
+
+    # That's all it is
+    assert_equal({ "move" => json['move'] }, json)
+  end
 end
