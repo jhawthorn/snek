c6f29eafe934a810977f6d821d4c4e4e34f00305
John Hawthorn <john@hawthorn.email>
2019-02-22 22:42:08 -0800

    Add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   post '/start', to: 'snake#start'
   post '/move',  to: 'snake#move'
   post '/end',   to: 'snake#end'
   post '/ping',  to: 'snake#ping'
+
+  root to: 'home#index'
 end
