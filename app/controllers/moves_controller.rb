400c62259ea3af8836aa9e0101c19d92bc5d45fa
John Hawthorn <john@hawthorn.email>
2021-09-25 19:25:10 -0700

    Use mlscorer in relays

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -4,7 +4,8 @@ class MovesController < ApplicationController
     @game = @move.game
 
     @simulate_game = ::Game.from_json(@move.state)
-    @move_decider = MoveDecider.new(@simulate_game)
+    scorer = ->(g) { MlScorer.new(g, weights: DefaultWeights) }
+    @move_decider = MoveDecider.new(@simulate_game, scorer: scorer)
     @simulated_move = @move_decider.next_move
   end
 end
